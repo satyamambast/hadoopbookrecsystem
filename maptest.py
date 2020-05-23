cba0d2cab6e89245dab2019bb6aee566bda8e9a3
satyamambast <satyamambast@github.com>
2020-05-23 14:21:32 +0530

    slow and steady changes

--- maptest.py
+++ maptest.py
@@ -2,6 +2,8 @@
 import pickle
 import sys
 #inp=sys.stdin
-#a=open('/home/satyam/hadoopbookrecsystem/new.txt','w+')
-data=pickle.load(sys.stdin.buffer)
-print(len(data.keys()))
+a=open('/home/satyam/hadoopbookrecsystem/new.txt','w+')
+a.write("hfasdadb")
+print(23)
+#data=pickle.load(sys.stdin.buffer)
+#print(len(data.keys()))
